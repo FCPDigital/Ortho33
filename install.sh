d392b0e5128bf9b1ef7d86e2d54758e813793359
SolalDR <solal.dussout-revel@hotmail.fr>
2017-11-06 04:49:18 -0800

    Remove some config files

--- install.sh
+++
@@ -1,5 +0,0 @@
-gem update --system
-gem install compass
-compass create --syntax sass --sass-dir "assets/sass" --css-dir "assets/css" --javascripts-dir "assets/js" --images-dir "assets/img"
-compass watch
-#sass --watch assets/sass/app.sass:assets/css/theme.css
